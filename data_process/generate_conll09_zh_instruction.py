0e836bbfb7ad3cd11e557e50faa6cec098d1e867
fangfang123gh <58161784+fangfang123gh@users.noreply.github.com>
2025-05-27 16:33:06 +0800

    Add files via upload

---
+++ data_process/generate_conll09_zh_instruction.py
@@ -0,0 +1,172 @@
+
+
+import json
+import copy
+import pickle
+from tqdm import tqdm
+def get_instruction_data(predicate_base_path, agent_path, data_path, save_path, require_pred=True, require_rl=True):
+    datas = []
+    with open(predicate_base_path, 'rb') as f:
+        preds_dict = pickle.load(f)
+
+    with open(agent_path, 'rb') as f:
+        pred_agent = pickle.load(f)
+
+    with open(data_path, 'r', encoding='utf-8') as f:
+        for line in tqdm(f.readlines()):
+            data = json.loads(line)
+            preds = data['srl']
+            key = data['text'] 
+            token = data['token']
+            text = key
+            
+            pr_str = key
+            sorted_result = sorted(preds, key=lambda x: x['position'][0])
+
+            count = 0
+            for a in sorted_result:
+                b, e = a['position']
+                if e == len(key):
+                    pr_str = pr_str[0: b-1 + count] + ' @@' + a['pred'] + '##'
+                    count += 5
+                elif b == 1:
+                    pr_str = '@@' + a['pred'] + '## ' + pr_str[e+count:]
+                    count += 5
+                else:
+                    pr_str = pr_str[0: b-1 + count] + ' @@' + a['pred'] + '## ' + pr_str[e+count:]
+                    count += 6
+
+            i = 0
+            maybe_pred_pos = []
+            maybe_pred_token = []
+            while i < len(token):
+                if token[i] in preds_dict:
+                    maybe_pred_token.append(token[i])
+                    maybe_pred_pos.append((len(''.join(token[:i])) +1, len(''.join(token[:i])) +len(token[i])))
+                        
+                i += 1
+            maybe_pred_pos = sorted(maybe_pred_pos, key=lambda x: x[0])
+            pred_agent_des = ''
+            all_maybe_pr_str = ''
+            if require_pred:
+                all_maybe_pr_str = text
+                count = 0
+                for a in maybe_pred_pos:
+                    # ar = "\"" + a['value'] + '[{},{}]'.format(a['position'][0],a['position'][1]) + "\""
+                    # arg_answer.append(ar)
+                    b, e = a
+                    value = text[b - 1 : e]
+                    if e == len(text):
+                        all_maybe_pr_str = all_maybe_pr_str[0: b-1 + count] + ' @@' + value + '##'
+                        count += 5
+                    elif b == 1:
+                        all_maybe_pr_str = '@@' + value + '## ' + all_maybe_pr_str[e+count:]
+                        count += 5
+                    else:
+                        all_maybe_pr_str = all_maybe_pr_str[0: b-1 + count] + ' @@' + value + '## ' + all_maybe_pr_str[e+count:]
+                        count += 6
+                
+                maybe_pred_token = set(maybe_pred_token)
+                maybe_pred_token = list(maybe_pred_token)
+                for t in maybe_pred_token:
+                    if t in pred_agent:
+                        pred_agent_des += f'当{t}作为谓词时，它的意思为：{", ".join(pred_agent[t])}\n'
+        
+
+        for r in preds:
+            conversations = []
+            task_exp = '语义角色标注（SRL）旨在识别句子中的谓词并为它们的论元分配角色。\n'
+            pred_exp = '谓词指的是句子中的核心词或短语，表示动作、事件或状态，并作为句子中其他成分的焦点。它通常是动词或形容词。\n'
+            conversations.append({'from': 'human', "value": task_exp + pred_exp})
+            conversations.append({"from": "gpt", "value": '我已经理解了这个任务。'})
+            if require_pred:
+                conversations.append({'from': 'human', "value": f'Text: {key}\n在进行语义角色标注任务时，给定文本中的谓词是什么？该文本中可能的谓词结果为: {all_maybe_pr_str}\n其中谓词由@@和##给定。\n结合给定的可能的谓词结果，请重新编写给定文本，并使用@@和##分别标记谓词的开头和结尾。注意没有出现在谓词结果的词也可能是谓词。\n'+pred_agent_des})
+            else:
+                conversations.append({'from': 'human', "value": f'Text: {key}\n在进行语义角色标注任务时，给定文本中的谓词是什么？请重新编写给定文本，并使用@@和##分别标记谓词的开头和结尾。'})
+            conversations.append({"from": "gpt", "value": pr_str})
+
+            instruction = "在语义角色标注中，论元指的是语义上与给定谓词相关的成分或短语。它进一步描述了与句子中谓词相关的实体、动作或概念。识别的论元应该是"
+            instruction += "论元分为核心论元和附加论元。\n"
+            instruction += "所有附加论元的标签如下：\n"
+            instruction += "ADV: Adverbial argument\nBNF: Beneficiary\nCND: Condition\nDIR: Direction\nEXT: Degree\nFRQ: Frequency\nLOC: Location\nMNR: Manner of action or event execution\nPRP: Purpose\nTMP: Time\nTPC: Topic\nDGR: Degree\nDIS: Discourse marker\nPRD: Sub-predicate\nVOC: Particle\n"
+            instruction += '对于复合短语，第一个短语的类型是论元的类型，例如A0，而后面的短语的类型是C-A0。'
+            pred = r['pred']
+            
+            instruction += "核心论元依赖于谓词，且谓词可能具有不同的核心论元框架。在这些框架内，核心论元会有不同的解释。\n"
+            conversations.append({'from': 'human', "value": instruction})
+            conversations.append({"from": "gpt", "value": '我已经理解了这个任务。'})
+            instruction = ''
+
+            position = r['position']
+            args = r['arguments']
+            text = key
+            if position[0] == 1:
+                text = '@@' + pred + '## ' + key[position[1]:] 
+            elif position[1] == len(key):
+                text = key[0:position[0]-1] + ' @@' + pred + '##'
+            else:
+                text = key[0:position[0]-1] + ' @@' + pred + '## ' + key[position[1]:] 
+            question = f"Text: {text}\n给定谓词的论元及其对应的角色是什么？谓词由@@和##给定。\n"
+            instruction += question
+            if require_rl:
+                frameset_str = ''
+                if pred in preds_dict:
+                    framesets = preds_dict[pred]
+                    for idx, frameset in enumerate(framesets, 1):
+                        frameset_str += f'框架 {idx}: \n它具有的核心论元是：\n'
+                        for frame_role, frame_exp in frameset.items():
+                            frameset_str += f"A{frame_role}: {frame_exp}\n"
+                if len(frameset_str) != 0:
+                    instruction += f'对于谓词"{pred}", 它具有以下框架：\n'
+                    instruction += frameset_str
+                    instruction += "通过参考提供的框架，确定谓词所属的框架，以确定其核心论元。\n"
+                    
+                else:
+                    instruction += "所有核心论元的标签如下：\n"
+                    instruction += "A0：执行动作或事件的实体\nA1：承受动作或事件的实体\nA2：根据谓词的不同，通常是动作或事件的目标或对象\nA3：根据谓词的不同，通常是动作或事件的间接接受者或受益者\nA4：根据谓词的不同，通常是动作或事件的来源或起源\n"
+            else:
+                instruction += "所有核心论元的标签如下：\n"
+                instruction += "A0：执行动作或事件的实体\nA1：承受动作或事件的实体\nA2：根据谓词的不同，通常是动作或事件的目标或对象\nA3：根据谓词的不同，通常是动作或事件的间接接受者或受益者\nA4：根据谓词的不同，通常是动作或事件的来源或起源\n"
+            instruction += "请重新编写给定文本，并使用相应的<label>和</label>标签将论元的开头和结尾括起来。\n"
+            arg_str = text
+            count = 0
+            flag = True
+            sorted_args = sorted(args, key=lambda x: x['position'][0])
+            for a in sorted_args:
+                
+                role = a['role']
+                b, e = a['position']
+                if b > position[0] and flag:
+                    if position[0] == 1:
+                        count += 5
+                    else:
+                        count += 6
+                    flag = False
+                
+                if e == len(key):
+                    arg_str = arg_str[0: b-1 + count] + ' <' + role + '>' + a['value'] + '</' + role + '>'
+                    count += len(' <' + role + '>' + '</' + role + '>')
+                elif b == 1:
+                    arg_str = '<' + role + '>' + a['value'] + '</' + role + '> ' + arg_str[e+count:]
+                    count += len('<' + role + '>' + '</' + role + '> ')
+                else:
+                    arg_str = arg_str[0: b-1 + count] + ' <' + role + '>' + a['value'] + '</' + role + '> ' + arg_str[e+count:]
+                    count += len(' <' + role + '>' + '</' + role + '> ')
+
+            conversations.append({"from": "human", "value": instruction})
+            gpt_template = {"from": "gpt", "value": arg_str}
+            conversations.append(gpt_template)
+            datas.append({'conversations': conversations, 'system': "你是一个有语言学背景并且善于理解文本，特别是在语义角色标注方面熟练的有帮助的助手。"})
+
+    json_data = json.dumps(datas,ensure_ascii=False)
+    with open(save_path, "w", encoding='utf-8') as file:
+        file.write(json_data)
+
+if __name__ == '__main__':
+    predicate_base_path = '' # the predicate database path
+    agent_path = '' # the predicate agent path
+    data_path = '' # the training data
+    get_instruction_data(predicate_base_path, agent_path, data_path)
+
+
+    
---
+++ data_process/generate_conll12_en_instruction.py
@@ -0,0 +1,266 @@
+import json
+import copy
+import pickle
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+from tqdm import tqdm
+from lemminflect import getLemma
+from nltk.corpus import wordnet
+
+def penn_to_wordnet(tag):
+    if tag.startswith('J'):
+        return wordnet.ADJ
+    elif tag.startswith('V'):
+        return wordnet.VERB
+    elif tag.startswith('N'):
+        return wordnet.NOUN
+    elif tag.startswith('R'):
+        return wordnet.ADV
+    else:
+        return None
+def get_instruction_data(predicate_base_path, agent_path, data_path, save_path, require_pred=True, require_rl=True):
+    datas = []
+    with open(predicate_base_path, 'rb') as f:
+        preds_dict = pickle.load(f)
+
+    with open(agent_path, 'rb') as f:
+        pred_agent = pickle.load(f)
+
+    with open(data_path, 'r', encoding='utf-8') as f:
+        for line in tqdm(f.readlines()):
+            data = json.loads(line)
+            preds = data['srl']
+            text = ' '.join(data['text'])
+            token = text.split()
+            lemmas = data['lemmas']
+            pos = data['pos']
+            
+            pr_str = copy.deepcopy(token)
+            sorted_result = sorted(preds, key=lambda x: x['position'][0])
+
+            # construct the gold-standard predicate recognition sequence
+            for a in sorted_result:
+                start, end = a['position']
+                pr_str[start - 1] ='@@'+ pr_str[start - 1]
+                pr_str[end - 1] =pr_str[end - 1] + '##'
+            pr_str = ' '.join(pr_str)
+
+            # construct the possible predicate recognition sequence
+            i = 0
+            maybe_pred_pos = []
+            maybe_pred_token = []
+            while i < len(token):
+                t = token[i].lower()
+                temp_lemma = lemmas[i]
+                # 先直接看在不在里面
+                if t in preds_dict:
+                    maybe_pred_pos.append(i)
+                    maybe_pred_token.append(t)
+                elif temp_lemma != '-':
+                    if temp_lemma in preds_dict:
+                        maybe_pred_pos.append(i)
+                        maybe_pred_token.append(temp_lemma)
+
+                else:
+                    
+                    lemma = getLemma(t, 'NOUN')
+                    for l in lemma:
+
+                        if l in preds_dict:
+                            maybe_pred_pos.append(i)
+                            maybe_pred_token.append(l)
+                            break
+                    if i not in maybe_pred_pos:
+                        lemma = getLemma(t, 'VERB')
+                        for l in lemma:
+
+                            if l in preds_dict:
+                                maybe_pred_token.append(l)
+                                maybe_pred_pos.append(i)
+                                break    
+                i += 1
+            pred_agent_des = ''
+            all_maybe_pr_str = ''
+            if require_pred:
+                all_maybe_pr_str = copy.deepcopy(token)
+                for a in maybe_pred_pos:
+                    all_maybe_pr_str[a] ='@@'+ all_maybe_pr_str[a] + '##'
+                all_maybe_pr_str = ' '.join(all_maybe_pr_str)
+
+                
+            
+                maybe_pred_token = set(maybe_pred_token)
+                maybe_pred_token = list(maybe_pred_token)
+                for t in maybe_pred_token:
+                    for key, value in pred_agent[t].items():
+                        temp = ''
+                        if len(value) != 0:
+                            pos_name = 'noun' if key == 'n' else 'verb'
+                            temp += f'When the {pos_name} "{t}" functions as a predicate, its interpretation is: '
+                
+                            unique_value = []
+                            for i,v in enumerate(value):
+                                find = False
+                                for j,vv in enumerate(value):
+                                    if i != j:
+                                        if v in vv or v== t:
+                                            find = True
+                                            break
+                                if not find:
+                                    unique_value.append(v)
+                            if len(unique_value) == 0:
+                                temp = ''
+                            else:
+                                temp += ", ".join(unique_value) + '\n'
+                        pred_agent_des += temp
+        
+
+        for r in preds:
+            conversations = []
+            task_exp = 'Semantic Role Labeling (SRL) aims to identify predicates in a sentence and assign roles to their arguments.\n'
+            pred_exp = 'A predicate refers to the core word or phrase in a sentence that conveys an action, event, or state and serves as the focus for other elements in the sentence.\n'
+            conversations.append({'from': 'human', "value": task_exp + pred_exp})
+            conversations.append({"from": "gpt", "value": 'I have understood this task.'})
+            
+            if require_pred:
+                conversations.append({'from': 'human', "value": f'Text: {" ".join(token)}\nFor the predicate indentification task, what are the predicates in the given text? Possible predicate results in the text are: {all_maybe_pr_str}\nwhere predicates are specified by @@ and ##.\n'+pred_agent_des+'Based on the given possible predicate results and interpretations, please rewrite the given text, marking the beginning and end of predicates with @@ and ## respectively. Note that words not present in the predicate results may also be predicates.\n'})
+            else:
+                conversations.append({'from': 'human', "value": f'Text: {" ".join(token)}\nFor the predicate indentification task, what are the predicates in the given text? Please rewrite the given text, marking the beginning and end of predicates with @@ and ## respectively.'})
+            # conversations.append({'from': 'human', "value": f'Text: {key}\n在进行语义角色标注任务时，给定文本中的谓词是什么？请重新编写给定文本，并使用@@和##分别标记谓词的开头和结尾。'})
+            conversations.append({"from": "gpt", "value": pr_str})
+
+            instruction = "In SRL, arguments refer to the components or phrases semantically related to a given predicate. They further describe the entities, actions, or concepts associated with the predicate in the sentence."
+            instruction += "Arguments are divided into core arguments and adjunct arguments.\n"
+            instruction += "The labels for all adjunct arguments are as follows:\n"
+            instruction += 'ARGM-EXT: extent\nARGM-LOC: location\nARGM-DIR: direction\nARGM-NEG: negation  (not in PREDITOR)\n'
+            instruction += 'ARGM-MOD: general modification\nARGM-ADV: adverbial modification\nARGM-MNR: manner\nARGM-PRD: secondary predication\n'
+            instruction += 'ARGM-REC: recipricol (eg herself, etc)\nARGM-TMP: temporal\nARGM-PRP: purpose\nARGM-PNC: purpose no cause\nARGM-CAU: cause\n'
+            instruction += 'ARGM-ADJ: adjectival (nouns only)\nARGM-COM: comitative\nARGM-DIS: discourse\nARGM-DSP: direct speech\n'
+            instruction += 'ARGM-GOL: goal\nARGM-LVB: light verb (for nouns only)\nARGA: secondary agent\nARGM-PRR: predicating relation\n'
+            pred = r['pred']
+            
+            instruction += "Core arguments depend on the predicate, and a predicate may have different core argument frames. Within these frames, core arguments will have different interpretations.\n"
+            conversations.append({'from': 'human', "value": instruction})
+            conversations.append({"from": "gpt", "value": 'I have understood this task.'})
+            instruction = ''
+
+            
+            args = r['arguments']
+            # instruction = ''
+            # instruction += conversations[0]['value']
+            start, end = r['position']
+            text = copy.deepcopy(token)
+
+            text[start - 1] = '@@' + text[start - 1]
+            text[end - 1] = text[end - 1] + '##'
+
+            question = f"Text: {' '.join(text)}\nWhat are the arguments and their corresponding roles for the given predicate? The predicate is specified by @@ and ##.\n"
+            instruction += question
+            
+            if require_rl:
+
+                # 框架的组织
+                frameset_str = ''
+                wn_tag = penn_to_wordnet(pos[start - 1])
+                v_or_n =  'v' if wn_tag == wordnet.VERB else None
+                if v_or_n is None:
+                    v_or_n =  'n' if wn_tag == wordnet.NOUN else None
+                framesets = {}
+                if lemmas[start - 1] != '-':
+                    lemma = lemmas[start - 1]
+                elif pred in ['\'s', '\'re', '\'m']:
+                    lemma = 'be'
+                else:
+                    
+                    lemma = lemmatizer.lemmatize(pred.lower(), pos=wn_tag)
+                if lemma not in preds_dict:
+                    lemma = lemmatizer.lemmatize(pred.lower(), pos=wn_tag)
+                if lemma in preds_dict:
+                    framesets = {}
+                    temp_word = lemma
+                    for j in range(1, 5):
+                        if start + j <= len(token):
+                            if token[start + j - 1] == '-':
+                                continue
+                            temp_word += ' ' + token[start + j - 1]
+                            if temp_word in preds_dict[lemma]:
+                                framesets[temp_word] = preds_dict[lemma][temp_word]
+                                break
+                    if len(framesets) == 0:
+                        if lemma in preds_dict[lemma]:
+                            framesets[lemma] = preds_dict[lemma][lemma]
+                if len(framesets) == 0:
+                    if pred in preds_dict:
+                        if pred in preds_dict[pred]:
+                            framesets[pred] = preds_dict[pred][pred]
+                        
+                        else:
+                            if len(preds_dict[pred]) != 0:
+                                for key, value in preds_dict[pred].items():
+                                    framesets[key] = value
+                    elif lemma in preds_dict:
+                        if lemma in preds_dict[lemma]:
+                            framesets[lemma] = preds_dict[lemma][lemma]
+                        else:
+                            if len(preds_dict[lemma]) != 0:
+                                for key, value in preds_dict[lemma].items():
+                                    framesets[key] = value
+                for frame_name, frameset in framesets.items():
+                    for key, value in frameset.items():
+                        if len(value) != 0:
+                            if v_or_n is not None and key == v_or_n:
+                                n = 'verb' if key == 'v' else 'noun'
+                                frameset_str += f'For {frame_name} as a {n}\n'
+                        
+                                for fram_index, f in enumerate(value):
+                                    if len(f) == 0:
+                                        continue
+                                    frameset_str += f'Frame {fram_index + 1}:\nThe core arguments it has are:\n'
+                                    for frame_role, frame_exp in f.items():
+                                        frameset_str += f"ARG{frame_role}: {frame_exp}\n"
+                            else:
+                                n = 'verb' if key == 'v' else 'noun'
+                                frameset_str += f'For {frame_name} as a {n}\n'
+                        
+                                for fram_index, f in enumerate(value):
+                                    if len(f) == 0:
+                                        continue
+                                    frameset_str += f'Frame {fram_index + 1}:\nThe core arguments it has are:\n'
+                                    for frame_role, frame_exp in f.items():
+                                        frameset_str += f"ARG{frame_role}: {frame_exp}\n" 
+                if len(frameset_str) != 0:
+                    instruction += f'For the predicate "{pred}" in this text, it has the following frames:\n'
+                    instruction += frameset_str
+                    instruction += "By referring to the provided frames, determine the frame to which the predicate belongs in order to identify its core arguments.\n"
+                
+                else:
+                    instruction += "The labels for all core arguments are as follows:\n"
+                    instruction += "ARG0: agent\nARG1: patient\nARG2: instrument, benefactive, attribute\nARG3: starting point, benefactive, attribute\nARG4: ending point\nARG5: depend on predicate"
+                
+            else:
+                instruction += "The labels for all core arguments are as follows:\n"
+                instruction += "ARG0: agent\nARG1: patient\nARG2: instrument, benefactive, attribute\nARG3: starting point, benefactive, attribute\nARG4: ending point\nARG5: depend on predicate"
+            instruction += '"R-" arguments are arguments that are referencing another argument in the sentence. "C-" arguments are discontinous spans that all refer to the same argument. Please rewrite the given text and enclose the beginning and end of the arguments with the corresponding <label> and </label> tags.\n'
+            sorted_args = sorted(args, key=lambda x: x['position'][0])
+            for a in sorted_args:
+                if a['role'] == 'ARGM-PRX':
+                    continue
+                role = a['role']
+                start, end = a['position']
+                text[start - 1] = f'<{role}>' + text[start - 1]
+                text[end - 1] = text[end - 1] + f'</{role}>'
+
+            conversations.append({"from": "human", "value": instruction})
+            gpt_template = {"from": "gpt", "value": ' '.join(text)}
+            conversations.append(gpt_template)
+            datas.append({'conversations': conversations , 'system': 'You are a helpful assistant who has a background in linguistics and is good at understanding texts, especially skilled in semantic role labeling recognition.'})
+
+    json_data = json.dumps(datas,ensure_ascii=False)
+    with open(save_path, "w", encoding='utf-8') as file:
+        file.write(json_data)
+
+if __name__ == '__main__':
+    predicate_base_path = '' # the predicate database path
+    agent_path = '' # the predicate agent path
+    data_path = '' # the training data
+    get_instruction_data(predicate_base_path, agent_path, data_path)
